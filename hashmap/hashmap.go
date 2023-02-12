fbc0331650713ccb80c8f7613475e83cb36812b3
wenxuan <864547554@qq.com>
2023-02-12 17:38:58 +0800

    hashmap: 242-有效的字母异位词

--- hashmap/hashmap.go
+++ hashmap/hashmap.go
@@ -1,1 +1,25 @@
 package hashmap
+
+// isAnagram
+// 242. 有效的字母异位词
+// https://leetcode.cn/problems/valid-anagram/
+func isAnagram(s string, t string) bool {
+	m := [26]int{}
+	for _, b := range []byte(s) {
+		m[b-'a']++
+	}
+
+	for _, b := range []byte(t) {
+		m[b-'a']--
+		if m[b-'a'] < 0 {
+			return false
+		}
+	}
+	for _, v := range m {
+		if v > 0 {
+			return false
+		}
+	}
+
+	return true
+}
--- hashmap/hashmap_test.go
+++ hashmap/hashmap_test.go
@@ -1,1 +1,26 @@
 package hashmap
+
+import "testing"
+
+func TestIsAnagram(t *testing.T) {
+	data1 := []string{
+		"anagram",
+		"aaaaabb",
+	}
+	data2 := []string{
+		"nagaram",
+		"aaaaab",
+	}
+	actual := []bool{
+		true,
+		false,
+	}
+
+	for i := range data1 {
+		result := isAnagram(data1[i], data2[i])
+		if result != actual[i] {
+			t.Fatalf("结果与实际不相符, caseIndex: %d, result: %v, actual: %v",
+				i, result, actual[i])
+		}
+	}
+}
