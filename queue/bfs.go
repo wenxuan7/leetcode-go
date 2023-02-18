7d2cd5d6efa098f835c13764179c63605a014b67
wenxuan <864547554@qq.com>
2023-02-18 16:18:19 +0800

    bfs: 102-二叉树的层序遍历

--- queue/bfs.go
+++ queue/bfs.go
@@ -1,1 +1,36 @@
 package queue
+
+import . "leetcode-go/data"
+
+// 102. 二叉树的层序遍历
+// https://leetcode.cn/problems/binary-tree-level-order-traversal/
+func levelOrder(root *TreeNode) [][]int {
+	if root == nil {
+		return [][]int{}
+	}
+
+	ans := make([][]int, 0)
+
+	q := make([]*TreeNode, 0)
+	q = append(q, root)
+
+	for len(q) > 0 {
+		level := make([]int, 0)
+
+		levelCount := len(q)
+		for i := 0; i < levelCount; i++ {
+			level = append(level, q[i].Val)
+			if q[i].Left != nil {
+				q = append(q, q[i].Left)
+			}
+			if q[i].Right != nil {
+				q = append(q, q[i].Right)
+			}
+		}
+
+		q = q[levelCount:]
+		ans = append(ans, level)
+	}
+
+	return ans
+}
--- queue/bfs_test.go
+++ queue/bfs_test.go
@@ -1,1 +1,21 @@
 package queue
+
+import (
+	"leetcode-go/assert"
+	"testing"
+)
+import . "leetcode-go/data"
+
+func TestLevelOrder(t *testing.T) {
+	data := []*TreeNode{
+		{Val: 3, Left: &TreeNode{Val: 9}, Right: &TreeNode{Val: 20, Left: &TreeNode{Val: 15}, Right: &TreeNode{Val: 7}}},
+	}
+	actual := [][][]int{
+		{{3}, {9, 20}, {15, 7}},
+	}
+
+	for i := range data {
+		result := levelOrder(data[i])
+		assert.Verify2Arr(t, i, result, actual[i])
+	}
+}
