2c93cdb0592af3a20d540b99c4736b7e478373a8
wenxuan <864547554@qq.com>
2023-02-23 22:42:46 +0800

    dfs: 200-岛屿数量

--- recursive/dfs.go
+++ recursive/dfs.go
@@ -1,1 +1,56 @@
 package recursive
+
+// numIslands
+// 200. 岛屿数量
+// https://leetcode.cn/problems/number-of-islands/
+func numIslands(grid [][]byte) int {
+	row, col := len(grid), len(grid[0])
+	set := make([][]bool, row)
+	for i := 0; i < row; i++ {
+		set[i] = make([]bool, col)
+	}
+
+	var dfs func(i, j int)
+	dfs = func(i, j int) {
+		if i < 0 || i >= row {
+			return
+		}
+		if j < 0 || j >= col {
+			return
+		}
+
+		if grid[i][j] == '1' && !set[i][j] {
+			grid[i][j] = '0'
+
+			dfs(i+1, j)
+			dfs(i-1, j)
+			dfs(i, j+1)
+			dfs(i, j-1)
+		} else {
+			return
+		}
+	}
+
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if grid[i][j] == '1' {
+				set[i][j] = true
+				dfs(i+1, j)
+				dfs(i-1, j)
+				dfs(i, j+1)
+				dfs(i, j-1)
+			}
+		}
+	}
+
+	ans := 0
+	for i := 0; i < row; i++ {
+		for j := 0; j < col; j++ {
+			if grid[i][j] == '1' {
+				ans++
+			}
+		}
+	}
+
+	return ans
+}
--- recursive/dfs_test.go
+++ recursive/dfs_test.go
@@ -1,1 +1,32 @@
 package recursive
+
+import (
+	"leetcode-go/assert"
+	"testing"
+)
+
+func TestNumIslands(t *testing.T) {
+	data := [][][]byte{
+		{
+			{'1', '1', '1', '1', '0'},
+			{'1', '1', '0', '1', '0'},
+			{'1', '1', '0', '0', '0'},
+			{'0', '0', '0', '0', '0'},
+		},
+		{
+			{'1', '1', '0', '0', '0'},
+			{'1', '1', '0', '0', '0'},
+			{'0', '0', '1', '0', '0'},
+			{'0', '0', '0', '1', '1'},
+		},
+	}
+	actual := []int{
+		1,
+		3,
+	}
+
+	for i := range data {
+		result := numIslands(data[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
