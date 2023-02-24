a28efb7ae474424cbf13fb3d6ac890765df48c2a
wenxuan <864547554@qq.com>
2023-02-25 00:06:54 +0800

    graph: 133-克隆图

--- graph/graph.go
+++ graph/graph.go
@@ -1,1 +1,48 @@
 package graph
+
+// Node 图节点
+type Node struct {
+	Val       int
+	Neighbors []*Node
+}
+
+// cloneGraph
+// 133. 克隆图
+// https://leetcode.cn/problems/clone-graph/
+func cloneGraph(node *Node) *Node {
+	if node == nil {
+		return nil
+	}
+
+	m := make(map[int]*Node, 0)
+
+	root := &Node{Val: node.Val}
+	var dfs func(node, newNode *Node)
+	dfs = func(node, newNode *Node) {
+		if m[node.Val] != nil {
+			return
+		}
+
+		m[node.Val] = newNode
+		if node.Neighbors == nil {
+			newNode.Neighbors = nil
+			return
+		}
+		newNeighbors := make([]*Node, len(node.Neighbors))
+		for i, neighbor := range node.Neighbors {
+			if m[neighbor.Val] != nil {
+				newNeighbors[i] = m[neighbor.Val]
+				continue
+			}
+
+			newNeighbor := &Node{Val: neighbor.Val}
+			newNeighbors[i] = newNeighbor
+			dfs(neighbor, newNeighbor)
+		}
+		newNode.Neighbors = newNeighbors
+	}
+
+	dfs(node, root)
+
+	return root
+}
