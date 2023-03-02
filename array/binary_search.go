a5115ebe0bc8718b1c7d8c2bebf8d5ea2d819ded
wenxuan <864547554@qq.com>
2023-03-02 21:02:05 +0800

    bs: 33-搜索旋转排序数组

--- array/binary_search.go
+++ array/binary_search.go
@@ -39,3 +39,36 @@ func isPerfectSquare(num int) bool {
 	}
 	return false
 }
+
+// search
+// 33. 搜索旋转排序数组
+// https://leetcode.cn/problems/search-in-rotated-sorted-array/
+func search(nums []int, target int) int {
+	if len(nums) == 0 {
+		return -1
+	}
+
+	l, r, mid := 0, len(nums)-1, 0
+	for l <= r {
+		mid = ((r - l) >> 1) + l
+		if nums[mid] == target {
+			return mid
+		}
+
+		if nums[l] <= nums[mid] {
+			if target >= nums[mid] || nums[l] > target {
+				l = mid + 1
+			} else {
+				r = mid - 1
+			}
+		} else if nums[mid] < nums[r] {
+			if target > nums[r] || nums[mid] > target {
+				r = mid - 1
+			} else {
+				l = mid + 1
+			}
+		}
+	}
+
+	return -1
+}
--- array/binary_search_test.go
+++ array/binary_search_test.go
@@ -38,3 +38,32 @@ func TestIsPerfectSquare(t *testing.T) {
 		assert.Verify(t, i, result, actual[i])
 	}
 }
+
+func TestSearch(t *testing.T) {
+	num := [][]int{
+		{4, 5, 6, 7, 0, 1, 2},
+		{4, 5, 6, 7, 0, 1, 2},
+		{1},
+		{1},
+		{3, 1},
+	}
+	target := []int{
+		0,
+		3,
+		0,
+		1,
+		1,
+	}
+	actual := []int{
+		4,
+		-1,
+		-1,
+		0,
+		1,
+	}
+
+	for i := range num {
+		result := search(num[i], target[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
