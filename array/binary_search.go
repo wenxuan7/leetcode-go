5007594475941432d8950a38abd343f9353552c0
wenxuan <864547554@qq.com>
2023-03-02 20:13:37 +0800

    bs: 69-x的平方根

--- array/binary_search.go
+++ array/binary_search.go
@@ -1,1 +1,21 @@
 package array
+
+// mySqrt
+// 69. x 的平方根
+// https://leetcode.cn/problems/sqrtx/
+func mySqrt(x int) int {
+	l, r, mid, product := 0, 46340, 0, 0
+
+	for l < r {
+		mid = (r-l+1)>>1 + l
+		product = mid * mid
+		if product > x {
+			r = mid - 1
+		} else if product == x {
+			return mid
+		} else {
+			l = mid
+		}
+	}
+	return l
+}
--- array/binary_search_test.go
+++ array/binary_search_test.go
@@ -1,1 +1,22 @@
 package array
+
+import (
+	"github.com/leetcode-go/assert"
+	"testing"
+)
+
+func TestMySqrt(t *testing.T) {
+	x := []int{
+		4,
+		8,
+	}
+	actual := []int{
+		2,
+		2,
+	}
+
+	for i := range x {
+		result := mySqrt(x[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
