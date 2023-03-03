2cec83fb9b5e85fa1c761cd061dc7d60388ab00c
wenxuan <864547554@qq.com>
2023-03-03 20:02:43 +0800

    dp: 62-不同路径

--- dp/dp.go
+++ dp/dp.go
@@ -1,1 +1,28 @@
 package dp
+
+// uniquePaths
+// 62. 不同路径
+// https://leetcode.cn/problems/unique-paths/
+func uniquePaths(m int, n int) int {
+	if m < 1 || n < 1 {
+		return 0
+	}
+
+	rows := make([][]int, m)
+	for i := 0; i < m; i++ {
+		rows[i] = make([]int, n)
+		if i == 0 {
+			rows[0][0] = 1
+		}
+		for j := 0; j < n; j++ {
+			if i-1 >= 0 {
+				rows[i][j] += rows[i-1][j]
+			}
+			if j-1 >= 0 {
+				rows[i][j] += rows[i][j-1]
+			}
+		}
+	}
+
+	return rows[m-1][n-1]
+}
--- dp/dp_test.go
+++ dp/dp_test.go
@@ -1,1 +1,26 @@
 package dp
+
+import (
+	"github.com/leetcode-go/assert"
+	"testing"
+)
+
+func TestUniquePaths(t *testing.T) {
+	m := []int{
+		3,
+		3,
+	}
+	n := []int{
+		7,
+		2,
+	}
+	actual := []int{
+		28,
+		3,
+	}
+
+	for i := range m {
+		result := uniquePaths(m[i], n[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
