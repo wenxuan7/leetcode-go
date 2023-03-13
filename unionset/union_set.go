a6131cc189d0d53a7d36575334ad26d772feb20a
wenxuan <864547554@qq.com>
2023-03-13 19:25:32 +0800

    feat: 更新README.md, 添加字符串题文件夹

--- unionset/union_set.go
+++ unionset/union_set.go
@@ -1,1 +1,9 @@
 package unionset
+
+// findCircleNum
+// 547. 省份数量
+// https://leetcode.cn/problems/number-of-provinces/
+func findCircleNum(isConnected [][]int) int {
+
+	return 0
+}
--- unionset/union_set_test.go
+++ unionset/union_set_test.go
@@ -1,1 +1,22 @@
 package unionset
+
+import (
+	"github.com/leetcode-go/assert"
+	"testing"
+)
+
+func TestFindCircleNum(t *testing.T) {
+	isConnected := [][][]int{
+		{{1, 1, 0}, {1, 1, 0}, {0, 0, 1}},
+		{{1, 0, 0}, {0, 1, 0}, {0, 0, 1}},
+	}
+	actual := []int{
+		2,
+		3,
+	}
+
+	for i := range isConnected {
+		result := findCircleNum(isConnected[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
