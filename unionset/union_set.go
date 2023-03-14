8de441448bb8908c6faff44255721e2c84dbc8d9
wenxuan <864547554@qq.com>
2023-03-14 22:55:10 +0800

    unionset: 200-岛屿数量

--- unionset/union_set.go
+++ unionset/union_set.go
@@ -41,3 +41,98 @@ func findCircleNum(isConnected [][]int) int {
 	}
 	return ans
 }
+
+// numIslands
+// 200. 岛屿数量
+// https://leetcode.cn/problems/number-of-islands/
+func numIslands(grid [][]byte) int {
+	if len(grid) == 0 {
+		return 0
+	}
+
+	us := NewUnionSet(grid)
+	m, n := len(grid), len(grid[0])
+	xy := [][]int{
+		{1, 0},
+		{0, 1},
+		{-1, 0},
+		{1, 0},
+	}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+
+			if grid[i][j] == '0' {
+				continue
+			}
+
+			grid[i][j] = '0'
+
+			for _, nums := range xy {
+				newI, newJ := i+nums[0], j+nums[1]
+				if newI >= 0 && newJ >= 0 &&
+					newI < m &&
+					newJ < n &&
+					grid[newI][newJ] == '1' {
+					us.Union(i*n+j, newI*n+newJ)
+				}
+			}
+
+		}
+	}
+
+	return us.GetCount()
+}
+
+type UnionSet struct {
+	nums  []int
+	rank  []int
+	count int
+}
+
+func NewUnionSet(grid [][]byte) UnionSet {
+	m, n := len(grid), len(grid[0])
+	us := UnionSet{nums: make([]int, m*n), rank: make([]int, m*n)}
+
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
+			if grid[i][j] == '0' {
+				continue
+			}
+
+			us.nums[i*n+j] = i*n + j
+			us.count++
+		}
+	}
+
+	return us
+}
+
+func (us *UnionSet) Find(x int) int {
+	if us.nums[x] != x {
+		us.nums[x] = us.Find(us.nums[x])
+	}
+
+	return us.nums[x]
+}
+
+func (us *UnionSet) Union(x, y int) {
+	xv, yv := us.Find(x), us.Find(y)
+
+	if xv != yv {
+		if us.rank[xv] > us.rank[yv] {
+			us.nums[yv] = xv
+		} else if us.rank[xv] < us.rank[yv] {
+			us.nums[xv] = yv
+		} else {
+			us.nums[yv] = xv
+			us.rank[xv] += 1
+		}
+
+		us.count--
+	}
+}
+
+func (us *UnionSet) GetCount() int {
+	return us.count
+}
--- unionset/union_set_test.go
+++ unionset/union_set_test.go
@@ -20,3 +20,29 @@ func TestFindCircleNum(t *testing.T) {
 		assert.Verify(t, i, result, actual[i])
 	}
 }
+
+func TestNumIslands(t *testing.T) {
+	data := [][][]byte{
+		{
+			{'1', '1', '1', '1', '0'},
+			{'1', '1', '0', '1', '0'},
+			{'1', '1', '0', '0', '0'},
+			{'0', '0', '0', '0', '0'},
+		},
+		{
+			{'1', '1', '0', '0', '0'},
+			{'1', '1', '0', '0', '0'},
+			{'0', '0', '1', '0', '0'},
+			{'0', '0', '0', '1', '1'},
+		},
+	}
+	actual := []int{
+		1,
+		3,
+	}
+
+	for i := range data {
+		result := numIslands(data[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
