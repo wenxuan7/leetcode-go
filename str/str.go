ad0486e0c80a97f1b815a46623931232f7e77e22
wenxuan <864547554@qq.com>
2023-03-21 22:50:06 +0800

    str: 字符串基础题

--- str/str.go
+++ str/str.go
@@ -1,1 +1,110 @@
 package str
+
+// toLowerCase
+// 709. 转换成小写字母
+// https://leetcode.cn/problems/to-lower-case/
+func toLowerCase(s string) string {
+	if s == "" {
+		return s
+	}
+
+	chs := make([]byte, len(s))
+	for i := range s {
+		if s[i] >= 'A' && s[i] <= 'Z' {
+			chs[i] = s[i] - 'A' + 'a'
+			continue
+		}
+		chs[i] = s[i]
+	}
+	return string(chs)
+}
+
+// lengthOfLastWord
+// 58. 最后一个单词的长度
+// https://leetcode.cn/problems/length-of-last-word/
+func lengthOfLastWord(s string) int {
+	if s == "" {
+		return 0
+	}
+
+	i, ans := len(s)-1, 0
+	for i > -1 {
+		if s[i] != ' ' {
+			break
+		}
+		i--
+	}
+
+	for i > -1 {
+		if s[i] != ' ' {
+			ans++
+		} else {
+			break
+		}
+		i--
+	}
+	return ans
+}
+
+// numJewelsInStones
+// 771. 宝石与石头
+// https://leetcode.cn/problems/jewels-and-stones/
+func numJewelsInStones(jewels string, stones string) int {
+	lower, upper := make([]bool, 26), make([]bool, 26)
+	for i := range jewels {
+		temp := jewels[i]
+		if temp >= 'A' && temp <= 'Z' {
+			upper[temp-'A'] = true
+			continue
+		}
+		lower[temp-'a'] = true
+	}
+
+	ans := 0
+	for i := range stones {
+		temp := stones[i]
+		if temp >= 'A' && temp <= 'Z' {
+			if upper[temp-'A'] {
+				ans++
+			}
+			continue
+		}
+
+		if lower[temp-'a'] {
+			ans++
+		}
+	}
+	return ans
+}
+
+// 387. 字符串中的第一个唯一字符
+// https://leetcode.cn/problems/first-unique-character-in-a-string/
+func firstUniqChar(s string) int {
+	if 0 == len(s) {
+		return -1
+	}
+
+	set := make([]int, 26)
+	for i := range set {
+		set[i] = len(s)
+	}
+	for i, ch := range s {
+		k := ch - 'a'
+		if set[k] == len(s) {
+			set[k] = i
+		} else {
+			set[k] = len(s) + 1
+		}
+	}
+
+	ans := len(s)
+	for _, num := range set {
+		if ans > num {
+			ans = num
+		}
+	}
+	if ans < len(s) {
+		return ans
+	}
+	return -1
+}
--- str/str_test.go
+++ str/str_test.go
@@ -1,1 +1,82 @@
 package str
+
+import (
+	"github.com/leetcode-go/assert"
+	"testing"
+)
+
+func TestToLowerCase(t *testing.T) {
+	s := []string{
+		"Hello",
+		"here",
+		"LOVELY",
+	}
+	actual := []string{
+		"hello",
+		"here",
+		"lovely",
+	}
+
+	for i := range s {
+		result := toLowerCase(s[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
+
+func TestLengthOfLastWord(t *testing.T) {
+	s := []string{
+		"Hello World",
+		"   fly me   to   the moon  ",
+		"luffy is still joyboy",
+	}
+	actual := []int{
+		5,
+		4,
+		6,
+	}
+
+	for i := range s {
+		result := lengthOfLastWord(s[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
+
+func TestNumJewelsInStones(t *testing.T) {
+	jewels := []string{
+		"aA",
+		"z",
+	}
+	stones := []string{
+		"aAAbbbb",
+		"ZZ",
+	}
+	actual := []int{
+		3,
+		0,
+	}
+
+	for i := range jewels {
+		result := numJewelsInStones(jewels[i], stones[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
+
+func TestFirstUniqChar(t *testing.T) {
+	s := []string{
+		"leetcode",
+		"loveleetcode",
+		"aabb",
+		"dddccdbba",
+	}
+	actual := []int{
+		0,
+		2,
+		-1,
+		8,
+	}
+
+	for i := range s {
+		result := firstUniqChar(s[i])
+		assert.Verify(t, i, result, actual[i])
+	}
+}
