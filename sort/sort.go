cb5b8b045ae9bebeaf0d965ce38130ad8e1cead2
wenxuan <864547554@qq.com>
2023-03-28 20:32:47 +0800

    sort: 75. 颜色分类

--- sort/sort.go
+++ sort/sort.go
@@ -1,1 +1,24 @@
 package sort
+
+// sortColors
+// 75. 颜色分类
+// https://leetcode.cn/problems/sort-colors/
+func sortColors(nums []int) {
+	if len(nums) == 0 {
+		return
+	}
+
+	p0, p2 := 0, len(nums)-1
+	for i := 0; i < len(nums); i++ {
+		for ; i <= p2 && nums[i] == 2; p2-- {
+			if nums[i] == 2 {
+				nums[i], nums[p2] = nums[p2], nums[i]
+			}
+		}
+
+		if nums[i] == 0 {
+			nums[i], nums[p0] = nums[p0], nums[i]
+			p0++
+		}
+	}
+}
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -1,1 +1,22 @@
 package sort
+
+import (
+	"github.com/leetcode-go/assert"
+	"testing"
+)
+
+func TestSortColors(t *testing.T) {
+	nums := [][]int{
+		{2, 0, 2, 1, 1, 0},
+		{2, 0, 1},
+	}
+	actual := [][]int{
+		{0, 0, 1, 1, 2, 2},
+		{0, 1, 2},
+	}
+
+	for i := range nums {
+		sortColors(nums[i])
+		assert.VerifyArr(t, i, nums[i], actual[i])
+	}
+}
