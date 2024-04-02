85cb5cfb0c50aeb74d0e035c089643efe02d330e
文选 <cebai.wx@raycloud.com>
2024-04-02 21:04:53 +0800

    test: addTwoNumbers添加测试用例

--- list/list_test.go
+++ list/list_test.go
@@ -1,1 +1,30 @@
 package list
+
+import (
+	. "github.com/leetcode/data"
+	"github.com/leetcode/require"
+	"testing"
+)
+
+func TestAddTwoNumbers(t *testing.T) {
+	data1 := []*ListNode{
+		require.HListNodeFromArray(4, 3, 5),
+		require.HListNodeFromArray(5, 5, 5),
+		nil,
+	}
+	data2 := []*ListNode{
+		require.HListNodeFromArray(5, 8, 7),
+		require.HListNodeFromArray(8, 8),
+		nil,
+	}
+	actual := []*ListNode{
+		require.HListNodeFromArray(9, 1, 3, 1),
+		require.HListNodeFromArray(3, 4, 6),
+		nil,
+	}
+
+	for i := range actual {
+		result := addTwoNumbers(data1[i], data2[i])
+		require.EqualListNode(t, i, result, actual[i])
+	}
+}
--- require/list.go
+++ require/list.go
@@ -2,113 +2,35 @@ package require
 
 import (
 	. "github.com/leetcode/data"
-	"strconv"
-	"strings"
 	"testing"
 )
 
-// GetListNode 根据索引获取链表节点
-func GetListNode(head *ListNode, i int) *ListNode {
-	var (
-		count = 0
-		curr  = head
-	)
-
-	for curr != nil {
-		if count == i {
-			return curr
-		}
-		curr = curr.Next
-		count++
-	}
-
-	return nil
-}
-
-// GenerateCycleList 生成环形链表， pos为成环索引位置
-func GenerateCycleList(num []int, pos int) *ListNode {
-	if len(num) == 0 {
+// HListNodeFromArray 从array生成链表，返回头节点
+func HListNodeFromArray(nums ...int) *ListNode {
+	if len(nums) == 0 {
 		return nil
 	}
 
-	var (
-		preHead = &ListNode{}
-		pre     = preHead
-		curr    *ListNode
-		posNode *ListNode
-		tail    *ListNode
-	)
+	preHead := &ListNode{}
+	pre := preHead
+	var curr *ListNode
 
-	for i := range num {
-		curr = &ListNode{Val: num[i], Next: nil}
-		pre.Next = curr
-		pre = curr
-
-		if i == pos {
-			posNode = curr
-		}
-		if i == len(num)-1 {
-			tail = curr
-		}
-	}
-
-	if posNode != nil {
-		tail.Next = posNode
-	}
-
-	return preHead.Next
-}
-
-// GenerateList 生成链表，返回头节点
-func GenerateList(num []int) *ListNode {
-	if len(num) == 0 {
-		return nil
-	}
-
-	var (
-		preHead           = &ListNode{}
-		pre               = preHead
-		curr    *ListNode = nil
-	)
-
-	for i := range num {
-		curr = &ListNode{Val: num[i], Next: nil}
+	for i := range nums {
+		curr = &ListNode{Val: nums[i], Next: nil}
 		pre.Next = curr
 		pre = curr
 	}
-
 	return preHead.Next
 }
 
-// ListToString 链表转字符串做格式化输出
-func ListToString(node *ListNode) string {
-	bd := strings.Builder{}
-	bd.WriteString("[")
-	for node != nil {
-		bd.WriteString(strconv.Itoa(node.Val))
-		if node.Next != nil {
-			bd.WriteString(", ")
-		}
-		node = node.Next
-	}
-	bd.WriteString("]")
-
-	return bd.String()
-}
-
-// VerifyList 校验两个链表的全部节点值是否相等
-func VerifyList(t *testing.T, caseIndex int, result *ListNode, actual *ListNode) {
+// EqualListNode 校验两个链表的全部节点值是否相等
+func EqualListNode(t *testing.T, caseIndex int, result *ListNode, actual *ListNode) {
 	if result == nil && actual == nil {
 		return
-	} else if result != nil && actual != nil {
-		if result.Val != actual.Val {
-			t.Fatalf("结果与实际不相符, caseIndex: %d, result: %s, actual: %s",
-				caseIndex, ListToString(result), ListToString(actual))
-		}
-
-		VerifyList(t, caseIndex, result.Next, actual.Next)
-	} else {
-		t.Fatalf("结果与实际不相符, caseIndex: %d, result: %s, actual: %s",
-			caseIndex, ListToString(result), ListToString(actual))
+	}
+	str1, str2 := result.String(), actual.String()
+	if str1 != str2 {
+		t.Fatalf("结果与实际不相符\ncaseIndex: %d\nresult: %s\nactual: %s",
+			caseIndex, str1, str2)
 	}
 }
