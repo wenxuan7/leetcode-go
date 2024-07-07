75e0fa2c47ec8bc4a84b2c4f7b5561f4e7a0f804
文选 <cebai.wx@raycloud.com>
2024-07-07 20:10:43 +0800

    tree: 200. 岛屿数量

--- graph/graph.go
+++ graph/graph.go
@@ -1,1 +1,34 @@
 package graph
+
+// numIslands 200. 岛屿数量
+// https://leetcode.cn/problems/number-of-islands/description/?envType=study-plan-v2&envId=top-interview-150
+func numIslands(grid [][]byte) int {
+	ret := 0
+	for r := 0; r < len(grid); r++ {
+		for c := 0; c < len(grid[r]); c++ {
+			if grid[r][c] == '0' {
+				continue
+			}
+			reset(grid, r, c)
+			ret++
+		}
+	}
+	return ret
+}
+
+func reset(grid [][]byte, r, c int) {
+	if r < 0 ||
+		c < 0 ||
+		r >= len(grid) ||
+		c >= len(grid[0]) {
+		return
+	}
+	if grid[r][c] == '0' {
+		return
+	}
+	grid[r][c] = '0'
+	reset(grid, r-1, c)
+	reset(grid, r+1, c)
+	reset(grid, r, c-1)
+	reset(grid, r, c+1)
+}
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -1,1 +1,32 @@
 package graph
+
+import (
+	"github.com/leetcode/require"
+	"testing"
+)
+
+func TestNumIslands(t *testing.T) {
+	data := [][][]byte{
+		{
+			{'1', '1', '1', '1', '0'},
+			{'1', '1', '0', '1', '0'},
+			{'1', '1', '0', '0', '0'},
+			{'0', '0', '0', '0', '0'},
+		},
+		{
+			{'1', '1', '0', '0', '0'},
+			{'1', '1', '0', '0', '0'},
+			{'0', '0', '1', '0', '0'},
+			{'0', '0', '0', '1', '1'},
+		},
+	}
+	actual := []int{
+		1,
+		3,
+	}
+
+	for i, grid := range data {
+		ret := numIslands(grid)
+		require.EqualNumber(t, i, ret, actual[i])
+	}
+}
