d5a8e5a4d1006281eaaca475deca24989eafd56a
文选 <cebai.wx@raycloud.com>
2024-07-09 21:56:07 +0800

    trie: 208. 实现 Trie (前缀树)

--- tree/trie.go
+++ tree/trie.go
@@ -1,27 +1,54 @@
 package tree
 
-import (
-	. "github.com/wenxuan7/leetcode/data"
-	"math"
-)
-
-var maxSum int
-
-// maxPathSum 124. 二叉树中的最大路径和
-// https://leetcode.cn/problems/binary-tree-maximum-path-sum/description/?envType=study-plan-v2&envId=top-interview-150
-func maxPathSum(root *TreeNode) int {
-	maxSum = math.MinInt
-	maxGain(root)
-	return maxSum
+// Trie 208. 实现 Trie (前缀树)
+// https://leetcode.cn/problems/implement-trie-prefix-tree/description/?envType=study-plan-v2&envId=top-interview-150
+type Trie struct {
+	children []*Trie
+	end      bool
 }
 
-func maxGain(root *TreeNode) int {
-	if root == nil {
-		return 0
-	}
+func Constructor() Trie {
+	return Trie{children: make([]*Trie, 26), end: false}
+}
 
-	l, r := max(maxGain(root.Left), 0), max(maxGain(root.Right), 0)
-	sum := root.Val + l + r
-	maxSum = max(maxSum, sum)
-	return root.Val + max(l, r)
+func (t *Trie) Insert(word string) {
+	pre, cur := t, t
+	idx := 0
+	for i := 0; i < len(word); i++ {
+		idx = int(word[i] - 'a')
+		if cur.children[idx] == nil {
+			pre = cur
+			cur = &Trie{children: make([]*Trie, 26), end: false}
+			pre.children[idx] = cur
+		} else {
+			cur = cur.children[idx]
+		}
+	}
+	cur.end = true
+}
+
+func (t *Trie) Search(word string) bool {
+	cur := t
+	idx := 0
+	for i := 0; i < len(word); i++ {
+		idx = int(word[i]) - 'a'
+		if cur == nil || cur.children[idx] == nil {
+			return false
+		}
+		cur = cur.children[idx]
+	}
+	return cur.end
+}
+
+func (t *Trie) StartsWith(prefix string) bool {
+	cur := t
+	idx := 0
+	for i := 0; i < len(prefix); i++ {
+		idx = int(prefix[i]) - 'a'
+		if cur == nil || cur.children[idx] == nil {
+			return false
+		}
+		cur = cur.children[idx]
+	}
+	return true
 }
