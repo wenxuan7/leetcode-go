13fc019e795524bdf34dacd5f8c559894fa6abec
文选 <cebai.wx@raycloud.com>
2024-07-10 20:43:37 +0800

    recursive: 46. 全排列

--- recursive/dfs.go
+++ recursive/dfs.go
@@ -30,3 +30,33 @@ func combine(n int, k int) [][]int {
 	dfs(1)
 	return ans
 }
+
+// permute 46. 全排列
+// https://leetcode.cn/problems/permutations/description/?envType=study-plan-v2&envId=top-interview-150
+func permute(nums []int) [][]int {
+	dp := make([]int, 0, len(nums))
+	ans := make([][]int, 0, len(nums))
+	appeared := make([]bool, len(nums))
+	var dfs func()
+	dfs = func() {
+		if len(dp) == len(nums) {
+			cp := make([]int, len(dp))
+			copy(cp, dp)
+			ans = append(ans, cp)
+			return
+		}
+		for i := 0; i < len(nums); i++ {
+			if appeared[i] {
+				continue
+			}
+			appeared[i] = true
+			dp = append(dp, nums[i])
+			dfs()
+			dp = dp[:len(dp)-1]
+			appeared[i] = false
+		}
+	}
+
+	dfs()
+	return ans
+}
