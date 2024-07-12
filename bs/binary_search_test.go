ffb0a82d2fb08504ae56f17e22c59b310c583b6e
文选 <cebai.wx@raycloud.com>
2024-07-12 19:47:34 +0800

    bs: 162. 寻找峰值

--- bs/binary_search_test.go
+++ bs/binary_search_test.go
@@ -1,5 +1,7 @@
 package bs
 
+import "math"
+
 // searchInsert 35. 搜索插入位置
 // https://leetcode.cn/problems/search-insert-position/description/?envType=study-plan-v2&envId=top-interview-150
 func searchInsert(nums []int, target int) int {
@@ -45,4 +47,34 @@ func searchMatrix(matrix [][]int, target int) bool {
 		}
 	}
 	return false
+}
+
+// findPeakElement 162. 寻找峰值
+// https://leetcode.cn/problems/find-peak-element/description/?envType=study-plan-v2&envId=top-interview-150
+func findPeakElement(nums []int) int {
+	n := len(nums)
+
+	// 辅助函数，输入下标 i，返回 nums[i] 的值
+	// 方便处理 nums[-1] 以及 nums[n] 的边界情况
+	get := func(i int) int {
+		if i == -1 || i == n {
+			return math.MinInt
+		}
+		return nums[i]
+	}
+
+	l, mid, r := 0, 0, n-1
+	mid1N, midN, midN1 := 0, 0, 0
+	for {
+		mid = l + (r-l)>>1
+		mid1N, midN, midN1 = get(mid-1), get(mid), get(mid+1)
+		if mid1N < midN && midN > midN1 {
+			return mid
+		}
+		if midN < midN1 {
+			l = mid + 1
+		} else {
+			r = mid - 1
+		}
+	}
 }
