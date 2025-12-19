96a829f20cf6826f30aef4e6c9fd3360ad6102e7
wenxuan <864547554@qq.com>
2025-12-19 23:28:20 +0800

    feat: 实现 合并排序 快速排序 计数排序 桶排序

--- alg/sort.go
+++ alg/sort.go
@@ -1,1 +1,123 @@
 package alg
+
+import "math/rand/v2"
+
+// MergeSort 合并排序
+func MergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	mid := len(arr) >> 1
+	left := MergeSort(arr[:mid])
+	right := MergeSort(arr[mid:])
+
+	res := make([]int, 0, len(arr))
+	l, r := 0, 0
+	for l < len(left) && r < len(right) {
+		if left[l] < right[r] {
+			res = append(res, left[l])
+			l++
+		} else {
+			res = append(res, right[r])
+			r++
+		}
+	}
+	res = append(res, left[l:]...)
+	res = append(res, right[r:]...)
+	return res
+}
+
+// QuickSort 快速排序
+func QuickSort(arr []int) {
+	if len(arr) < 2 {
+		return
+	}
+
+	moveRandomToFirst(arr)
+	pivot, l := 0, 1
+	for i := 1; i < len(arr); i++ {
+		if arr[i] < arr[pivot] {
+			arr[l], arr[i] = arr[i], arr[l]
+			l++
+		}
+	}
+	arr[pivot], arr[l-1] = arr[l-1], arr[pivot]
+
+	QuickSort(arr[:l])
+	QuickSort(arr[l:])
+}
+
+func moveRandomToFirst(arr []int) {
+	n := rand.IntN(len(arr))
+	arr[0], arr[n] = arr[n], arr[0]
+}
+
+// CountingSort 计数排序
+func CountingSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	mn, mx := arr[0], arr[0]
+	for i := 1; i < len(arr); i++ {
+		mn = min(mn, arr[i])
+		mx = max(mx, arr[i])
+	}
+
+	L := mx - mn + 1
+	cnt := make([]int, L)
+	for i := 0; i < len(arr); i++ {
+		cnt[arr[i]-mn]++
+	}
+	for i := 1; i < L; i++ {
+		cnt[i] += cnt[i-1]
+	}
+
+	res := make([]int, len(arr))
+	num, sub, idx := 0, 0, 0
+	for r := len(arr) - 1; r >= 0; r-- {
+		num = arr[r]
+		sub = num - mn
+		idx = cnt[sub]
+
+		cnt[sub] = idx - 1
+		res[idx-1] = num
+	}
+	return res
+}
+
+// BucketSort 桶排序
+func BucketSort(arr []int, bucketSize int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+
+	mn, mx := arr[0], arr[0]
+	for i := 1; i < len(arr); i++ {
+		mn = min(mn, arr[i])
+		mx = max(mx, arr[i])
+	}
+
+	// (mx-mn+1-1)/bucketSize + 1
+	bucketCnt := (mx-mn)/bucketSize + 1
+	buckets := make([][]int, bucketCnt)
+	idx := 0
+	for _, num := range arr {
+		idx = (num - mn) / bucketSize
+		buckets[idx] = append(buckets[idx], num)
+	}
+
+	res := make([]int, 0, len(arr))
+	var sorted []int
+	for _, nums := range buckets {
+		sorted = CountingSort(nums)
+		res = append(res, sorted...)
+	}
+	return res
+}
+
+// RadixSort 基数排序
+func RadixSort(arr []int) []int {
+	return nil
+}
--- alg/sort_test.go
+++ alg/sort_test.go
@@ -1,1 +1,25 @@
 package alg
+
+import "testing"
+
+func TestSort(t *testing.T) {
+	numsArr := [][]int{
+		{5, 4, 3, 2, 1},
+		{10, 11, 8, 12, 7, 6, 5, 4, 1, 3, 2},
+		{33, 43, 1, 10, 5, 2, 1},
+	}
+
+	for _, nums := range numsArr {
+		merge := MergeSort(nums)
+		t.Log("merge:", merge)
+
+		count := CountingSort(nums)
+		t.Log("count:", count)
+
+		bucket := BucketSort(nums, 5)
+		t.Log("bucket:", bucket)
+
+		QuickSort(nums)
+		t.Log("quick:", nums)
+	}
+}
